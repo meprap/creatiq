3b0ddc0cff1220bb211430c0a17a5a0fc0f4e0ab
Pradip Shrestha <pradipshrestha@Pradips-MacBook-Pro.local>
2018-12-13 23:26:19 +0545

    changes

--- dev/scripts/scripts.js
+++ dev/scripts/scripts.js
@@ -1,5 +1,20 @@
 $(document).ready(function () {
 
+	var forEach=function(t,o,r){if("[object Object]"===Object.prototype.toString.call(t))for(var c in t)Object.prototype.hasOwnProperty.call(t,c)&&o.call(r,t[c],c,t);else for(var e=0,l=t.length;l>e;e++)o.call(r,t[e],e,t)};
+
+    var hamburgers = document.querySelectorAll(".hamburger");
+    if (hamburgers.length > 0) {
+      forEach(hamburgers, function(hamburger) {
+        hamburger.addEventListener("click", function() {
+          this.classList.toggle("is-active");
+        }, false);
+      });
+	}
+	
+	$('.hamburger').on('click',function(){
+		$('.theme-header').toggleClass('theme-header--is-open');
+	});
+
 	$('.panel-group--design-1 .panel').on('show.bs.collapse', function () {
 		$(this).addClass('active');
 	});
